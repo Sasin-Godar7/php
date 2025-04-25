306d3a4ead8373164f7dcab3692cf10d14666a61
Sasin Godar <sago_bca2080@lict.edu.np>
2025-04-25 14:56:32 +0545

    updated

--- FileReading_Student.php
+++ FileReading_Student.php
@@ -6,35 +6,30 @@
     <title>Document</title>
 </head>
 <body>
-    <!--reading student record in table form-->
-    <fieldset style="width: 300px; border: 2px solid black; padding: 10px;">
-        <legend>Student Record</legend>
-        <table border="1" cellpadding="5" cellspacing="0">
-            <tr>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Grade</th>
-            </tr>
-            <?php
-            if(file_exists("studentrecord.txt")) {
-                $file = fopen("studentrecord.txt", "r");
-                while(!feof($file)) {
-                    $line = fgets($file);
-                    if(trim($line) != "") {
-                        list($name, $age, $grade) = explode(",", $line);
-                        echo "<tr>";
-                        echo "<td>" . trim(str_replace("Name: ", "", $name)) . "</td>";
-                        echo "<td>" . trim(str_replace("Age: ", "", $age)) . "</td>";
-                        echo "<td>" . trim(str_replace("Grade: ", "", $grade)) . "</td>";
-                        echo "</tr>";
-                    }
-                }
-                fclose($file);
-            } else {
-                echo "<tr><td colspan='3'>No records found.</td></tr>";
-            }
-            ?>
-        </table>
+  <?php
+  if(file_exists("Student.txt"))
+  {
+    $fp = fopen("Student.txt", "r") or die("Unable to open file!");
+echo "<h2>Student records:</h2>";
+<table border='1'>
+<tr><th>Name</th><th>Age</th><th>Grade</th></tr>";
+while(!feof($fp))
+{
+$str = fgets($fp);
+$arr= explode(",",$str);
+
+if(arr[0] != ""){
+echo "<tr><td>".$arr[0]."</td><td>".$arr[1]."</td><td>".$arr[2]."</td></tr>";
+
+  }
+
+  echo "</table>";
+  fclose($fp);
+    }
+    else
+    {
+        echo "student file  not found!";
+    }
     
     
 
