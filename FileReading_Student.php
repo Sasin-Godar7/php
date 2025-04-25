7c2cbdab5a4fd8314b93c4e1f7b3ca6cfb480d86
Sasin Godar <sago_bca2080@lict.edu.np>
2025-04-25 15:01:52 +0545

    update

--- FileReading_Student.php
+++ FileReading_Student.php
@@ -7,29 +7,24 @@
 </head>
 <body>
   <?php
-  if(file_exists("studentrecord.txt"))
-  
-    $fp = fopen("Student.txt", "r") or die("Unable to open file!");
-echo "<h2>Student records:</h2>";
-<table border='1'>
-<tr><th>Name</th><th>Age</th><th>Grade</th></tr>";
-while(!feof($fp))
-{
-$str = fgets($fp);
-$arr= explode(",",$str);
+if(file_exists("studentrecord.txt")) {
+  $fp = fopen("studentrecord.txt", "r") or die("Unable to open file!");
+  echo "<h2>Student records:</h2>";
+  echo "<table border='1'>";
+  echo "<tr><th>Name</th><th>Age</th><th>Grade</th></tr>";
+  while(!feof($fp)) {
+      $str = fgets($fp);
+      $arr = explode(",", $str);
 
-if(arr[0] != ""){
-echo "<tr><td>".$arr[0]."</td><td>".$arr[1]."</td><td>".$arr[2]."</td></tr>";
-
+      if($arr[0] != "") {
+          echo "<tr><td>".$arr[0]."</td><td>".$arr[1]."</td><td>".$arr[2]."</td></tr>";
+      }
   }
-
   echo "</table>";
   fclose($fp);
-    }
-    else
-    {
-        echo "student file  not found!";
-    }
+} else {
+  echo "Student file not found!";
+}
     
     
 
