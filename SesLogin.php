45f90e16355bf0bb00ce9ea6a2ece9c9a48127a9
Sasin Godar <sago_bca2080@lict.edu.np>
2025-04-30 15:26:04 +0545

    update

---
+++ SesLogin.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+if (isset($_SESSION['username'])) {
+    header("Location: dashboard.php");
+    exit();
+}
+?>
+
+<form method="POST" action="login.php">
+    <label>Username:</label>
+    <input type="text" name="username" required><br>
+
+    <label>Password:</label>
+    <input type="password" name="password" required><br>
+
+    <input type="submit" name="login" value="Login">
+</form>
+
+<?php
+if (isset($_POST['login'])) {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    // Dummy credentials
+    $validUser = "admin";
+    $validPass = "1234";
+
+    if ($user === $validUser && $pass === $validPass) {
+        $_SESSION['username'] = $user;
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        echo "<p style='color:red;'>Invalid username or password.</p>";
+    }
+}
+?>
