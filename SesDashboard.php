cd967a3427239bb33c2e7bc6a35104b3edf13cf5
Sasin Godar <sago_bca2080@lict.edu.np>
2025-04-30 15:29:27 +0545

    update

---
+++ SesDashboard.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
+<p>This is your dashboard.</p>
+<a href="logout.php">Logout</a>
+<br><br>
